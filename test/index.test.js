3fd0f20ac3e29459b30e137cbb84d8ac5b5d1754
anemoi42 <anemoi42@gmail.com>
2025-01-17 01:59:39 +0900

    instructionSet.js fix

--- test/index.test.js
+++ test/index.test.js
@@ -77,3 +77,53 @@ test("Firis-Test03", () => {
   const firis = new Firis()
   expect(firis.createStringContainer("↓")).toBe(false)
 });
+
+test("Firis-Test04", () => {
+  const firis = new Firis()
+  expect(firis.createStringContainer("えんじ")).toBe(true)
+
+  expect(firis.inputKey('e')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('j')).toBe(0)
+  expect(firis.inputKey('i')).toBe(2)
+});
+
+test("Firis-Test05", () => {
+  const firis = new Firis()
+  expect(firis.createStringContainer("あんどんなっんあ")).toBe(true)
+
+  expect(firis.inputKey('a')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('d')).toBe(0)
+  expect(firis.inputKey('o')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('a')).toBe(1)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('a')).toBe(0)
+  expect(firis.inputKey('x')).toBe(0)
+  expect(firis.inputKey('t')).toBe(0)
+  expect(firis.inputKey('u')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('n')).toBe(0)
+  expect(firis.inputKey('a')).toBe(2)
+
+});
+
+test("Firis-Test06", () => {
+  const firis = new Firis()
+  expect(firis.createStringContainer("しかっちゃ")).toBe(true)
+
+  expect(firis.inputKey('s')).toBe(0)
+  expect(firis.inputKey('h')).toBe(0)
+  expect(firis.inputKey('i')).toBe(0)
+  expect(firis.inputKey('c')).toBe(0)
+  expect(firis.inputKey('a')).toBe(0)
+  expect(firis.inputKey('c')).toBe(0)
+  expect(firis.inputKey('t')).toBe(1)
+  expect(firis.inputKey('c')).toBe(0)
+  expect(firis.inputKey('h')).toBe(0)
+  expect(firis.inputKey('a')).toBe(2)
+
+});
